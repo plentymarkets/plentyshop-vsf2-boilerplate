57d1bbfb28b7fffe1bd411dcbc71d73ceee1d817
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-01-25 14:13:47 +0200

    fix: fixed selectshipping method on initial load

--- packages/theme/components/Checkout/VsfShippingProvider.vue
+++ packages/theme/components/Checkout/VsfShippingProvider.vue
@@ -20,7 +20,7 @@
 
 <script>
 import { SfButton, SfRadio, SfCheckbox } from '@storefront-ui/vue';
-import { ref, computed, onMounted } from '@nuxtjs/composition-api';
+import { ref, computed, watch } from '@nuxtjs/composition-api';
 import { keyBy } from 'lodash';
 import {
   useShippingProvider,
@@ -39,14 +39,12 @@ export default {
     const selectedMethodId = ref(null);
     const selectedMethod = ref(null);
     const shippinngPrivacyCheck = ref(false);
+    const initialSelection = ref(true);
     const {
       save,
-      state: shippingProvider
-    } = useShippingProvider();
+      state: shippingProvider, loading: loadingShippingProvider } = useShippingProvider();
     const { load: loadPaymentProviders } = usePaymentProvider();
     const { cart } = useCart();
-    console.log('shippingProvider');
-    console.log(shippingProvider.value);
     const shippingMethods = computed(() => shippingProviderGetters.getShippingProviders(shippingProvider.value));
     const shippingMethodsById = computed(() => keyBy(shippingMethods.value, 'parcelServicePresetId'));
     if (shippingProviderGetters.getShippingProfileId(cart?.value)) {
@@ -58,32 +56,29 @@ export default {
       selectedMethodId.value = shippingProviderGetters.getParcelServicePresetId(method);
       await loadPaymentProviders();
     };
-    onMounted(async () => {
-      //   problems:
-      //   1. if i have the selectedMethodId how do i get the selectedMethod object in a clean code manner
-      // is there a server endpoint to get a shipping method object based on a shippingmethod id ?
-      //   2. my solution with shippingMethodsById is not good for the case when: the user clears app cache and restart process of purchase
-      //     -add to cart .... navigate to checkout tab. i don't have shippingProvider.value, so implicit no shippingMethods.
-      // but if you refresh everything working...
-      //   is this a bug ???
-      // console.log('onMounted');
-      // console.log(shippingProvider.value);
-      // console.log(shippingMethods.value);
-      // console.log(shippingMethodsById.value);
-      // console.log(selectedMethodId.value);
-      selectedMethodId.value = shippingProviderGetters.getShippingProfileId(cart?.value);
-      // the case when you navigate first time to checkout
-      if (shippingMethodsById.value && shippingMethodsById.value.length) {
+    watch(loadingShippingProvider, async (newLoading) => {
+      if (!newLoading && initialSelection.value) {
+        initialSelection.value = false;
+        selectedMethodId.value = shippingProviderGetters.getShippingProfileId(cart?.value);
         const method = shippingMethodsById.value[selectedMethodId.value];
         await selectMethod(method);
       }
-    });
+    }, { immediate: true });
+
+    // onMounted(async () => {
+    //   if (Object.keys(shippingMethods.value).length) {
+    //     selectedMethodId.value = shippingProviderGetters.getShippingProfileId(cart?.value);
+    //     const method = shippingMethodsById.value[selectedMethodId.value];
+    //     await selectMethod(method);
+    //   }
+    // });
 
     const changeHint = (val) => {
       emit('shippingPrivacyHintAccepted', val);
     };
 
     return {
+      loadingShippingProvider,
       shippinngPrivacyCheck,
       shippingMethods,
       selectedMethodId,
