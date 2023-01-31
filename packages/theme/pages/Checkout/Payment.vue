a875c0f05aa1b0bfcf87e8c43456cad8cbb34a66
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-01-31 11:20:39 +0100

    Update packages/theme/pages/Checkout/Payment.vue
    Co-authored-by: Kevin Stederoth <43753494+ksted@users.noreply.github.com>

--- packages/theme/pages/Checkout/Payment.vue
+++ packages/theme/pages/Checkout/Payment.vue
@@ -63,7 +63,7 @@
         <SfCheckbox v-e2e="'terms'" v-model="terms" name="terms" class="summary__terms">
           <template #label>
             <div class="sf-checkbox__label">
-              {{ $t('I agree to') }} <SfLink link="#"> {{ $t('Terms and conditions') }}</SfLink>
+              {{ $t('Payment.I agree to') }} <SfLink link="#"> {{ $t('Payment.Terms and conditions') }}</SfLink>
             </div>
           </template>
         </SfCheckbox>
