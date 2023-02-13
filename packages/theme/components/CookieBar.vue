41fe619a2fd890642fa166bb3b8b780b491d6018
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-02-13 17:06:55 +0200

    fix: improvements

--- packages/theme/components/CookieBar.vue
+++ packages/theme/components/CookieBar.vue
@@ -1,22 +1,28 @@
 <template>
   <client-only>
     <div>
-      <!-- cookiebar -->
-      <div :class="!furtherSettingsOn ? 'cookieControl' : 'cookieControl2'">
-        <div class="card" v-if="!cookieGroups.decided">
+      <div
+        v-if="!cookieGroups.decided"
+        :class="!furtherSettingsOn ? 'cookieGroupCard' : 'furtherSettingsOn'"
+      >
+        <div class="card p-xs">
           <div v-if="!furtherSettingsOn">
-            <div class="sf-heading__description p-2">
-              {{ cookieGroups.text.barTitle }}
+            <!-- cookie info -->
+            <div class="sf-heading">
+              {{ cookieGroups.barTitle }}
             </div>
-            <div class="barDescription p-2">
-              {{ cookieGroups.text.barDescription }}
-              <a href="#">Privacy Settings</a>
+            <div class="barDescription p-xs">
+              {{ cookieGroups.barDescription }}
+
+              <SfButton link="/privacy-policy" class="sf-button--text">
+                Privacy Settings
+              </SfButton>
             </div>
-            <div class="flex p-2">
+            <!-- checkboxes -->
+            <div class="checkboxes ml-xs">
               <div
                 v-for="cookieGroup in cookieGroups.list"
                 :key="cookieGroup.id"
-                class="item"
               >
                 <SfCheckbox
                   :name="cookieGroup.name"
@@ -26,20 +32,16 @@
               </div>
             </div>
           </div>
-          <div
-            style="overflow-y: scroll; background: white; height: 300px"
-            v-else
-          >
-            <div style="height: 200px">
+          <div class="furtherSettingsCard" v-else>
+            <div class="furtherSettingsCardScrollable">
               <div
                 v-for="cookieGroup in cookieGroups.list"
                 :key="cookieGroup.id"
-                class="bg-grey"
-                style="margin-bottom: 5px"
+                class="bg-grey mb-xs"
               >
-                <div class="p-2">
+                <div class="p-sm">
                   <SfCheckbox
-                    style="color: green"
+                  class=""
                     :name="`${cookieGroup.name}(${cookieGroup.cookies.length})`"
                     :label="cookieGroup.name"
                     v-model="cookieGroup.accepted"
@@ -47,42 +49,59 @@
                   <div class="cookieDescription">
                     {{ cookieGroup.description }}
                   </div>
-                  <div style="margin-left:10px;" v-if="cookieGroup.showMore">
+                  <div v-if="cookieGroup.showMore" class="ml-xs cookieDetails">
                     <div
                       v-for="(cookie, index) in Object.entries(
                         cookieGroup.cookies
                       )"
                       :key="index"
                     >
-                      <div v-for="k in Object.keys(cookie[1])" :key="k">
-                        {{ k }}: {{ cookie[1][k] }}
+                      <div class="flex full-width mb-xs p-xs bg-white"  v-for="keyName in Object.keys(cookie[1])" :key="keyName">
+                        <div v-if="keyName !== 'name'" class="col-2">{{ keyName }}</div>
+                        <div class="col-4">
+                          {{ cookie[1][keyName] }}
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <a
-                    style="text-decoration: underline"
-                    href="#"
+                  <SfButton
                     v-if="!cookieGroup.showMore"
                     @click="cookieGroup.showMore = true"
-                    >More information</a
+                    class="sf-button--text"
                   >
-                  <a href="#" v-else @click="cookieGroup.showMore = false"
-                    >Show less</a
+                    More information
+                  </SfButton>
+                  <SfButton
+                    v-else
+                    @click="cookieGroup.showMore = false"
+                    class="sf-button--text"
                   >
+                    Show less
+                  </SfButton>
                 </div>
               </div>
             </div>
           </div>
-
-          <div v-if="!furtherSettingsOn"  style="text-align:center">
-            <a @click="furtherSettingsOn = true">Further Settings</a>
+          <!-- further settings / back button -->
+          <div class="furtherSettingMargin text-center">
+            <SfButton
+              v-if="!furtherSettingsOn"
+              @click="furtherSettingsOn = true"
+              class="sf-button--text"
+            >
+              Further Settings
+            </SfButton>
+            <SfButton
+              v-else
+              @click="furtherSettingsOn = false"
+              class="sf-button--text"
+            >
+              Back
+            </SfButton>
           </div>
-          <div v-else style="text-align:center">
-            <a href="#" @click="furtherSettingsOn = false">back</a>
-          </div>
-          <!-- buttons -->
-          <div class="flex">
-            <div class="item p-2">
+          <!-- action buttons -->
+          <div class="actionButtons">
+            <div class="actionButton">
               <button
                 class="color-primary full-width sf-button"
                 :aria-disabled="false"
@@ -93,9 +112,9 @@
                 Accept ALL
               </button>
             </div>
-            <div class="item p-2">
+            <div class="actionButton">
               <button
-                class="color-primary full-width sf-button"
+                class="color-primary full-width  sf-button"
                 :aria-disabled="false"
                 type="button"
                 aria-label="button"
@@ -104,12 +123,11 @@
                 REJECT ALL
               </button>
             </div>
-            <div class="item p-2">
+            <div class="actionButton">
               <button
-                class="full-width sf-button flat"
+                class="sf-button full-width  flat"
                 :aria-disabled="false"
                 type="button"
-                aria-label="button"
                 @click="toggle(false, true)"
               >
                 Accept SELECTION
@@ -118,13 +136,20 @@
           </div>
         </div>
       </div>
-
+      <!-- button to open cookie tab -->
       <button
-          v-if="cookieGroups.decided"
-          class="color-primary sf-button openCookies"
-          aria-label="Cookie control"
-          @click="cookieGroups.decided = false"
-        >
+        v-else
+        class="color-primary sf-button openCookies"
+        aria-label="Cookie control"
+        @click="cookieGroups.decided = false"
+      >
+        <SfIcon
+          icon="info_shield"
+          size="sm"
+          color="white"
+          viewBox="0 0 24 24"
+          :coverage="1"
+        />
         cookies
       </button>
     </div>
@@ -133,16 +158,18 @@
 
 <script>
 import { ref, useContext } from '@nuxtjs/composition-api';
-import { SfModal, SfCheckbox } from '@storefront-ui/vue';
+import { SfModal, SfCheckbox, SfIcon, SfButton } from '@storefront-ui/vue';
 export default {
   components: {
     SfModal,
-    SfCheckbox
+    SfIcon,
+    SfCheckbox,
+    SfButton
   },
   setup() {
     const { app } = useContext();
     const furtherSettingsOn = ref(false);
-    console.log(app.$cookies.getAll());
+    // cookieGroups must be defined in nuxtconfig and language plugins
     const cookieGroups = ref({
       modal: false,
       decided: false,
@@ -296,30 +323,17 @@ export default {
           showMore: false
         }
       ],
-      barPosition: 'bottom-right',
-      text: {
-        barTitle: 'We value your privacy',
-        barDescription:
-          'We use cookies and similar technologies on our website and process personal data of visitors to our website (e.g. IP address), e.g. to personalise content and advertisements, to integrate media from third-party providers or to analyse access to our website. Data processing only takes place when cookies are set. We share this data with third parties that we name in the settings.Data processing may be carried out with consent or on the basis of a legitimate interest. Consent can be given or refused. There is a right not to consent and to change or withdraw consent at a later date. We provide more information about the use of personal data and the services in our',
-        toggle: 'Accept all',
-        declineAll: 'Delete all',
-        manageCookies: 'Manage cookies',
-        unsaved: 'You have unsaved settings',
-        close: 'Close',
-        save: 'Save',
-        necessary: 'Necessary cookies',
-        optional: 'Optional cookies',
-        functional: 'Functional cookies',
-        blockedIframe: 'To see this, please enable functional cookies',
-        here: 'here',
-        furtherSettingsOn: false,
-        acceptNecessary: 'Accept necessary'
-      }
+      barTitle: 'We value your privacy',
+      barDescription:
+        'We use cookies and similar technologies on our website and process personal data of visitors to our website (e.g. IP address), e.g. to personalise content and advertisements, to integrate media from third-party providers or to analyse access to our website. Data processing only takes place when cookies are set. We share this data with third parties that we name in the settings.Data processing may be carried out with consent or on the basis of a legitimate interest. Consent can be given or refused. There is a right not to consent and to change or withdraw consent at a later date. We provide more information about the use of personal data and the services in our'
     });
 
     cookieGroups.value.decided = app.$cookies.getAll().decided;
-    if (app.$cookies.getAll().cookieGroups && app.$cookies.getAll().cookieGroups.length) {
-      cookieGroups.value.list.forEach(cookieGroup => {
+    if (
+      app.$cookies.getAll().cookieGroups &&
+      app.$cookies.getAll().cookieGroups.length
+    ) {
+      cookieGroups.value.list.forEach((cookieGroup) => {
         if (app.$cookies.getAll().cookieGroups.includes(cookieGroup.id)) {
           cookieGroup.accepted = true;
         }
@@ -327,12 +341,14 @@ export default {
     }
     const toggle = (all, state) => {
       if (all) {
-        cookieGroups.value.list.forEach(cookieGroup => {
+        cookieGroups.value.list.forEach((cookieGroup) => {
           cookieGroup.accepted = state;
         });
       }
 
-      const list = cookieGroups.value.list.filter(x => x.accepted).map(x=> x.id);
+      const list = cookieGroups.value.list
+        .filter((x) => x.accepted)
+        .map((x) => x.id);
       app.$cookies.set('cookieGroups', JSON.stringify(list), {
         path: '/',
         maxAge: 60 * 60 * 24 * 7
@@ -349,93 +365,186 @@ export default {
 };
 </script>
 
-<style scoped>
-.flex {
-  display: flex; /* or inline-flex */
+<style lang="scss" scoped>
+.cookieGroupCard {
+  width: 604px;
+  position: fixed;
+  bottom: 0;
+  right: var(--spacer-xs);
+  z-index: 1000;
+  height: 362px;
+  color: rgb(0, 0, 0);
+  font-weight: var(--font-weight--medium);
+  font-size: var(--font-size--xl);
+  font-family: 'Raleway', sans-serif;
+  @include for-mobile {
+    min-height: 70vh;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    z-index: 1000;
+    right: var(--spacer-xs);
+  }
 }
-.bg-grey {
-  background: #f2f2f4;
-}
-.item {
-  flex-grow: 1; /* default 0 */
-}
-.full-width {
+.furtherSettingsOn {
   width: 100%;
+  position: fixed;
+  bottom: 0;
+  height: 400px;
+  z-index: 1000;
+  color: rgb(0, 0, 0);
+  font-family: 'Raleway', sans-serif;
+  font-weight: var(--font-weight--medium);
+  font-size: var(--font-size--xl);
+  @include for-mobile {
+    min-height: 70vh;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    z-index: 1000;
+    right: var(--spacer-xs);
+  }
 }
-.p-2 {
-  padding: 5px;
+.furtherSettingsCard {
+  overflow-y: scroll;
+  background: white;
+  height: 300px;
 }
-.row {
-  flex-direction: row;
+.furtherSettingsCardScrollable {
+  height: 250px;
 }
-.sf-heading__description {
+.sf-heading {
+  text-align: center;
+  @include for-mobile {
+    text-align: left;
+    margin-left: var(--spacer-xs);
+  }
   font-family: 'Raleway';
   font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 26px;
-  /* identical to box height, or 144% */
+  font-weight: var(--font-weight--semibold);
+  padding: var(--spacer-xs) 0 0 0;
+  font-size: var(--font-size--xl);
   color: #7ccb83;
 }
 .barDescription {
   font-family: 'Raleway';
   font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 140.6%;
+  font-weight: var(--font-weight--normal);
+  font-size: var(--font-size--sm);
+  line-height: 1.6;
+  @include for-mobile {
+    font-size: 3vw;
+  }
 }
-.cookieControl {
-  width: 604px;
-  position: fixed;
-  bottom: 350px;
-  right: 20px;
-  z-index: 1000;
-  height: 30px;
-  text-align: center;
-  line-height: 30px;
-  color: rgb(0, 0, 0);
-  font-size: 14px;
-  font-family: 'Lato', sans-serif;
-  font-weight: 100;
-  transition: 0.8s;
-  animation: slideIn 0.8s;
-  animation-delay: 0.8s;
-}
+
 .openCookies {
   position: fixed;
   bottom: 0;
-  right: 20px;
-}
-.cookieControl2 {
-  width: 100%;
-  position: fixed;
-  bottom: 80px;
-  height: 300px;
-  z-index: 1000;
-  line-height: 30px;
-  color: rgb(0, 0, 0);
-  font-size: 14px;
-  font-family: 'Lato', sans-serif;
-  font-weight: 100;
-  transition: 0.8s;
-  animation: slideIn 0.8s;
-  animation-delay: 0.8s;
+  right: var(--spacer-xs);
 }
 
-.cookieControl .card {
+.card {
   background: white;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 .cookieDescription {
   font-family: 'Raleway';
   font-style: normal;
-  font-weight: 300;
-  font-size: 14px;
-  line-height: 140.6%;
+  font-weight: var(--font-weight--normal);
+  font-size: var(--font-size--sm);
+  line-height: 1.6;
+}
+.text-center {
+  text-align: center;
 }
 .flat {
   background: white;
   border: 1px solid green;
   color: black;
 }
+.flex-center {
+  align-items: center;
+}
+.flex {
+  width: 100%;
+  display: flex;
+}
+.bg-grey {
+  background: #f2f2f4;
+}
+.checkboxmargin {
+  margin: 0 var(--spacer-sm) 0 var(--spacer-sm);
+}
+
+.col {
+  flex-grow: 1;
+}
+.col-2 {
+  flex: 2;
+}
+.col-4 {
+  flex: 4;
+}
+.full-width {
+  width: 100%;
+}
+.p-sm {
+  padding: var(--spacer-sm);
+}
+.p-xs {
+  padding: var(--spacer-xs);
+}
+.px-xs {
+  padding: 0 var(--spacer-xs);
+}
+.mx-xs {
+  margin: 0 var(--spacer-xs);
+}
+.row {
+  flex-direction: row;
+}
+.mb-xs {
+  margin: 0 0 var(--spacer-xs) 0;
+}
+.ml-xs {
+  margin: 0 0 0 var(--spacer-xs);
+}
+.furtherSettingMargin {
+  margin: var(--spacer-xs) 0 var(--spacer-sm) 0;
+}
+.cookieDetails {
+  font-weight: var(--font-weight--light);
+}
+.bg-white {
+  background:white
+}
+.actionButtons {
+  display: flex;
+  flex-direction: row;
+
+  @include for-mobile {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+}
+.actionButton {
+  flex: 1;
+  padding: 0 var(--spacer-xs) 0 var(--spacer-xs);
+  text-align: center;
+  @include for-mobile {
+    width: 100%;
+    margin-bottom: var(--spacer-xs);
+  }
+}
+.checkboxes {
+  display: flex;
+  @include for-mobile {
+    width: 100%;
+  }
+}
+.cookieGroupCheckbox {
+  width: 50vw;
+}
 </style>
+
