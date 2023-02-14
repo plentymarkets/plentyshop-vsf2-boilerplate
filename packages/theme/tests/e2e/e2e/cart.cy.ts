bfc22d4589cd2dc59acef3970c6549e1989b89d9
ksted <kevin.stederoth@plentymarkets.com>
2023-02-14 13:05:35 +0100

    fix: add item via api

--- packages/theme/tests/e2e/e2e/cart.cy.ts
+++ packages/theme/tests/e2e/e2e/cart.cy.ts
@@ -3,20 +3,14 @@ import page from '../pages/factory';
 context('Cart', () => {
   beforeEach(function init () {
     page.home.visit();
+    page.home.addCartItem(1100, 1);
 
   });
 
   it(['happyPath', 'regression'], 'Should clear the cart', function test() {
-    cy.intercept('/api/plentymarkets/addCartItem').as('addCartItem');
     cy.intercept('/api/plentymarkets/clear').as('clearCart');
     cy.intercept('/api/plentymarkets/getProduct').as('getProduct');
 
-    // page.home.addCartItem(1100, 1);
-    cy.get('[data-e2e*="app-header"]').eq(1).find('a').click();
-    page.category.products.first().click().wait('@getProduct');
-
-    page.product.addToCartButton.click();
-    cy.wait('@addCartItem');
     page.product.header.openCart();
     cy.get('[data-e2e="clear-cart"]').click();
     cy.wait('@clearCart');
