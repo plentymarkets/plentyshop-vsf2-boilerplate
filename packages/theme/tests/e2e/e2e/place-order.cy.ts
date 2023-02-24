ebfac3f0062f05c7be827050a5d1f5700f98e996
ksted <kevin.stederoth@plentymarkets.com>
2023-02-24 07:23:04 +0100

    fix: add missing intercepts

--- packages/theme/tests/e2e/e2e/place-order.cy.ts
+++ packages/theme/tests/e2e/e2e/place-order.cy.ts
@@ -1,5 +1,6 @@
 import page from '../pages/factory';
 import { acceptCookies } from '../pages/utils/cookie';
+
 context('Order placement', () => {
   beforeEach(function init () {
     cy.fixture('customer').then((fixture) => {
@@ -12,16 +13,17 @@ context('Order placement', () => {
   });
   it(['happyPath', 'regression'], 'Should successfully place an order as a guest user', function test () {
     const data = this.fixtures.data;
-
     cy.intercept('/api/plentymarkets/addCartItem').as('addCartItem');
     cy.intercept('/api/plentymarkets/additionalInformation').as('additionalInformation');
     cy.intercept('/api/plentymarkets/deleteCart').as('deleteCart');
     cy.intercept('/api/plentymarkets/executePayment').as('executePayment');
     cy.intercept('/api/plentymarkets/getActiveShippingCountries').as('getActiveShippingCountries');
+    cy.intercept('/api/plentymarkets/getFacet').as('getFacet');
     cy.intercept('/api/plentymarkets/getPaymentProviders').as('getPaymentProviders');
     cy.intercept('/api/plentymarkets/getProduct').as('getProduct');
     cy.intercept('/api/plentymarkets/getShippingProvider').as('getShippingProvider');
     cy.intercept('/api/plentymarkets/loadAddresses').as('loadAddresses');
+    cy.intercept('/api/plentymarkets/loginUser').as('loginUser');
     cy.intercept('/api/plentymarkets/placeOrder').as('placeOrder');
     cy.intercept('/api/plentymarkets/preparePayment').as('preparePayment');
     cy.intercept('/api/plentymarkets/saveAddress').as('saveAddress');
@@ -30,7 +32,10 @@ context('Order placement', () => {
     // With the current data, the first category does not have items. Therefore, we need to replace the
     // following selector: page.home.header.categories.first().click();
     cy.get('[data-e2e*="app-header"]').eq(1).find('a').click();
-    page.category.products.first().click().wait('@getProduct');
+    cy.wait('@getFacet');
+
+    page.category.products.first().click();
+    cy.wait('@getProduct');
 
     page.product.addToCartButton.click();
     cy.wait('@addCartItem');
@@ -39,12 +44,9 @@ context('Order placement', () => {
     page.cart.goToCheckoutButton.click();
 
     page.checkout.checkoutlogin.continueAsUser(data.customer);
-    cy.wait(['@registerUser', '@getActiveShippingCountries']);
-
-    page.checkout.billing.url();
+    cy.wait(['@registerUser', '@loginUser', '@getActiveShippingCountries']);
 
     page.checkout.billing.fillForm(data.customer);
-    // page.checkout.billing.createAddress.click();
     page.checkout.billing.continueToShipping.click();
     cy.wait(['@saveAddress', '@loadAddresses', '@getActiveShippingCountries']);
 
@@ -53,9 +55,9 @@ context('Order placement', () => {
 
     page.checkout.shipping.continueToPaymentButton.click();
     cy.wait(['@saveAddress', '@loadAddresses', '@getShippingProvider', '@getPaymentProviders']);
-
     cy.get('[data-e2e*="shipping-method"]').should('exist');
     cy.get('[data-e2e*="payment-method"]').should('exist');
+
     page.checkout.payment.paymentMethods.first().click();
     page.checkout.payment.terms.click();
     page.checkout.payment.makeAnOrderButton.click();
