682b6c9a0ac44acfc3ebc5aa69c3ec54b170b56d
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-03-21 12:52:49 +0200

    fix: user obj is not updated

--- packages/composables/src/useGuest/index.ts
+++ packages/composables/src/useGuest/index.ts
@@ -1,11 +1,11 @@
-import { ref, Ref, computed } from '@nuxtjs/composition-api';
+import { Ref, ComputedRef, computed } from '@nuxtjs/composition-api';
 import type { User, Guest } from '@vue-storefront/plentymarkets-api';
 
 export const useGuest = (
   user: Ref<User>
 ): Guest => {
-  const isGuest: Ref<boolean> = computed(() => {
-    return user.value !== null && (user.value?.guestMail ?? false);
+  const isGuest: ComputedRef<boolean> = computed(() => {
+    return user.value !== null && user.value.guestMail !== null;
   });
 
   return {
