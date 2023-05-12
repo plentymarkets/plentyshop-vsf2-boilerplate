0dc8bdb6a83997a84d8f1911479fbc571d5eb9be
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-05-12 16:44:14 +0300

    test: testing commit email

--- packages/theme/tests/e2e/e2e/checkout.cy.ts
+++ packages/theme/tests/e2e/e2e/checkout.cy.ts
@@ -102,8 +102,6 @@ context('Check Thank You Page', () => {
     page.checkout.thankyou.itemsTable.should('be.visible');
     cy.get('[data-e2e*="order-item-product-name"]').should('be.visible');
 
-
-
     page.checkout.thankyou.documentsList.should('be.visible');
     page.checkout.thankyou.orderSummary.should('be.visible');
     page.checkout.thankyou.paymentSummary.should('be.visible');
