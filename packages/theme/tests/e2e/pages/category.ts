07ca9632e90e564e858213c369376f473d1f2234
ksted <kevin.stederoth@plentymarkets.com>
2023-05-14 12:40:15 +0200

    refactor: cart tests with stubs

--- packages/theme/tests/e2e/pages/category.ts
+++ packages/theme/tests/e2e/pages/category.ts
@@ -4,6 +4,16 @@ class Category {
   get products(): Cypress.Chainable {
     return el('category-product-card', 'a');
   }
+
+  get productToCartButton(): Cypress.Chainable {
+    return cy.get('[v-e2e="category-product-card"]').first().within(() => {
+      cy.get('[data-testid="product-add-icon"]').click();
+    })
+  }
+
+  visit(category: string): Cypress.Chainable {
+    return cy.visit(category);
+  }
 }
 
 export default new Category();
--- packages/theme/tests/e2e/pages/components/header.ts
+++ packages/theme/tests/e2e/pages/components/header.ts
@@ -42,6 +42,24 @@ class Header {
       expect(Cypress.$('[v-e2e="login-modal"]')).to.exist;
     });
   }
+
+  login(isRememberChecked = false): void {
+    const uniqueEmail = `e2etestemail-${new Date().getTime()}@plentymarkets.com`;
+    const password = 'Testuser1234';
+
+    this.openAccount();
+    if (uniqueEmail) {
+      this.accountModalForm.find('input#email').clear().type(uniqueEmail, { force: true });
+    }
+    if (password) {
+      this.accountModalForm.find('input#password').clear().type(password, { force: true });
+    }
+    if (isRememberChecked) {
+      this.accountModalForm.find('label.sf-checkbox__container').click();
+    }
+  
+    this.accountModalForm.find('button[type=submit]').click();
+  }
 }
 
 export default new Header();
