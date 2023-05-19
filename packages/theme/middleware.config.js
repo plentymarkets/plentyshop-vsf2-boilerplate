480cc56ccef459beb3bcc05cd3a3627a6a2c0aab
Bedeoan Raul <bedeoan.raul@gmail.com>
2023-05-19 09:25:37 +0300

    fix: set old endpoint back

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -4,8 +4,7 @@ module.exports = {
       location: '@vue-storefront/plentymarkets-api/server',
       configuration: {
         api: {
-          // url: process.env.API_ENDPOINT || 'https://mevofvd5omld.c01-14.plentymarkets.com'
-          url: process.env.API_ENDPOINT || 'https://msql6u6bz7bl.c14-01.plentymarkets.com'
+          url: process.env.API_ENDPOINT || 'https://mevofvd5omld.c01-14.plentymarkets.com'
         }
       },
       extensions: extensions => [...extensions]
