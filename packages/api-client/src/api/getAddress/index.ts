fc0f996d341fb6414df6fe7595e68cb2fe6ec733
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-05-19 09:03:25 +0200

    fix: lint warning

--- packages/api-client/src/api/getAddress/index.ts
+++ packages/api-client/src/api/getAddress/index.ts
@@ -14,6 +14,7 @@ export async function loadAddresses(context: Context, typeId: AddressType): Prom
   url.searchParams.set('addressTypeId', typeId.toString());
 
   const { data } = await context.client.get(url.href);
+
   return (data as AddressData[]).map(addressData => mapAddressForClient(addressData)) || [];
 }
 
