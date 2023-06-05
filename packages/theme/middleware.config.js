1d7647116b9119a2b52f8ec027b0908f3466c667
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-06-05 19:59:10 +0200

    chore: remove middleware

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -4,7 +4,7 @@ module.exports = {
       location: '@vue-storefront/plentymarkets-api/server',
       configuration: {
         api: {
-          url: process.env.API_ENDPOINT || 'https://wsiapyd5960w.c14-01.plentymarkets.com'
+          url: process.env.API_ENDPOINT || 'https://mevofvd5omld.c01-14.plentymarkets.com'
         }
       },
       extensions: extensions => [...extensions]
