dc66c1e3597cbcbb41893f38334cdb2ccfb6e63f
rbadici-plenty <121930969+rbadici-plenty@users.noreply.github.com>
2023-06-14 10:52:38 +0300

    Delete index.d.ts

--- packages/pp-integration/server/composables/useSmartButtonOrder/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const useSmartButtonOrder: () => {
-    createOrderSmartButton: (fundingSource: string) => Promise<string>;
-    loading: import("@vue/composition-api").ComputedRef<any>;
-    error: import("@vue/composition-api").ComputedRef<any>;
-};
