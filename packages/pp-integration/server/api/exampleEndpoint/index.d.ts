bb1bb42c79c152ec5a28b22a31efaaa80915d2c8
rbadici-plenty <121930969+rbadici-plenty@users.noreply.github.com>
2023-06-14 10:52:06 +0300

    Delete index.d.ts

--- packages/pp-integration/server/api/exampleEndpoint/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PaypalIntegrationContext } from '../../types';
-export declare function createOrder(context: PaypalIntegrationContext, fundingSource: string): Promise<any>;
-export declare function getSession(context: PaypalIntegrationContext, initialRestCall: boolean): Promise<any>;
-export default createOrder;
