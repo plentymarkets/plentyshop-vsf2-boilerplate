0def72e7eb47d66916589be9fb563fb8c72776e4
rbadici-plenty <121930969+rbadici-plenty@users.noreply.github.com>
2023-06-14 10:52:46 +0300

    Delete consts.d.ts

--- packages/pp-integration/server/consts.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const API_WEBHOOK_ENDPOINT = "webhook";
-export declare const LOGGER_PREFIX = "[VSF2Paypalplentymarkets]";
-export declare const MALFORMED_PRICE_QUERY_STRING = "malformed-price";
