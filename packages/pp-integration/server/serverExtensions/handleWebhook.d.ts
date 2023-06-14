40b435023004df33b24eacae7ad8b395f63e49e4
rbadici-plenty <121930969+rbadici-plenty@users.noreply.github.com>
2023-06-14 10:53:33 +0300

    Delete handleWebhook.d.ts

--- packages/pp-integration/server/serverExtensions/handleWebhook.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response } from 'express';
-import { WebhookClient } from 'src/types';
-declare const handleWebhook: (client: WebhookClient, config: any /** TODO */) => (request: Request, response: Response) => Promise<Response<any, Record<string, any>>>;
-export default handleWebhook;
